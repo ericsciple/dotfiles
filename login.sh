2f5250ebf310fbf50b9a1f66b85179a103a98608
eric sciple <ericsciple@users.noreply.github.com>
2021-01-17 22:39:58 -0600

    .

--- login.sh
+++ login.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 set -e
-set -v
 
 echo "Enter your Octofactory API key"
 read octofactory_key
 echo "Enter your GitHub PAT (requires read:packages)"
 read github_pat
+
+set -v
+
 docker login octofactory.githubapp.com -u ericsciple -p "$octofactory_key"
 docker login docker.pkg.github.com -u ericsciple -p "$github_pat"
 docker login containers.pkg.github.com -u ericsciple -p "$github_pat"
