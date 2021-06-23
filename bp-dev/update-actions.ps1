9192f08eb2bac09a37d0fab055e597757d182a6e
eric sciple <ericsciple@github.com>
2021-06-23 22:32:06 +0000

    .

--- bp-dev/update-actions.ps1
+++ bp-dev/update-actions.ps1
@@ -1,1 +1,7 @@
+$stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+
 deploy actions.l2 -UseExistingDatabases
+
+$stopwatch.Stop()
+Write-Host ""
+Write-Host ("Time Elapsed {0:hh\:mm\:ss}" -f $stopwatch.Elapsed)  
